8e535ee506bd85d1a129f80980a90e30f0121726
fsarachu <fsarachu@gmail.com>
2017-04-06 20:36:29 -0300

    Add App header navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <template>
     <div id="app">
-
-        <section class="section">
-            <div class="container">
-
-                <h1 class="title has-text-centered">Vue Router!</h1>
-
-                <hr>
-
-                <router-view></router-view>
-
-            </div>
-        </section>
-
+        <app-header></app-header>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-    export default {}
+    import AppHeader from './components/Header.vue';
+
+    export default {
+        components: {
+            AppHeader
+        }
+    }
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="nav has-shadow">
+        <div class="container">
+
+            <div class="nav-left">
+                <a class="nav-item is-tab is-active">Home</a>
+                <a class="nav-item is-tab">User</a>
+            </div>
+
+        </div>
+    </nav>
+</template>
