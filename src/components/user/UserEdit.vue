8dd2c4b840f4b32e948e815137c19a4b8a041062
fsarachu <fsarachu@gmail.com>
2017-04-06 22:23:55 -0300

    Pass and fetch query parameters

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -1,9 +1,26 @@
 <template>
-    <section class="hero is-light is-fullheight">
+    <section class="hero is-light">
         <div class="hero-body">
             <div class="container">
                 <h1 class="title">The UserEdit Component</h1>
+                <ul>
+                    <li>UserId: {{ userId }}</li>
+                    <li>Locale: {{ locale }}</li>
+                </ul>
             </div>
         </div>
     </section>
 </template>
+
+<script>
+    export default {
+        computed: {
+            userId() {
+                return this.$route.params.id;
+            },
+            locale() {
+                return this.$route.query.locale;
+            }
+        }
+    }
+</script>
