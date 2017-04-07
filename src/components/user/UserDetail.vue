07055d23d77401ac24186c2ea236bcc18f7442ed
fsarachu <fsarachu@gmail.com>
2017-04-06 22:06:25 -0300

    Add edit user button

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -4,6 +4,8 @@
             <div class="container has-text-centered">
                 <h1 class="title is-4">The UserDetail Component</h1>
                 <h2 class="subtitle">UserId: {{ userId }}</h2>
+
+                <router-link :to="`/user/${userId}/edit`" tag="button" class="button is-info">Edit User</router-link>
             </div>
         </div>
     </section>
